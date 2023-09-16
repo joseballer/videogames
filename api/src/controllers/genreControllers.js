8f41a518ac6ee9ff9e1cf63a6322a810c9c846a9
joseballer <joseballera@gmail.com>
2023-09-16 18:46:47 -0400

    sigo con las rutas

--- api/src/controllers/genreControllers.js
+++ api/src/controllers/genreControllers.js
@@ -6,7 +6,7 @@ const getAllGenres = async () => {
   let allGenres = await Genre.findAll();
   if (allGenres.length === 0) {
     const response = await axios.get(`${GENRE_URL}?key=${API_KEY}`);
-    allGenres= response.data.results.map((genre) => ({
+    allGenres = response.data.results.map((genre) => ({
       Nombre: genre.name,
     }));
     await Genre.bulkCreate(allGenres);
--- api/src/handlers/genreHandlers.js
+++ api/src/handlers/genreHandlers.js
@@ -1,9 +1,9 @@
 const { getAllGenres } = require('../controllers/genreControllers')
 
 const getGenresHandler = async (req, res) => {
-	//const { genre } = req.query
+	const { genre } = req.query
 	try {
-		const response =  await getAllGenres()
+		const response =  await getAllGenres(genre)
 		return res.status(201).json(response)
 	} catch (error) {
 		return res.status(400).json({ error: error.message })
